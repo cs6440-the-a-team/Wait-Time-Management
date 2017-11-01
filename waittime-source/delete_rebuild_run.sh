21ccb9ff54359a249884cb00bec3092b66947270
Alain Brown <alain.brown@gmail.com>
2017-11-01 14:42:03 -0700

    fix docker cleanup script

--- waittime-source/delete_rebuild_run.sh
+++ waittime-source/delete_rebuild_run.sh
@@ -2,8 +2,8 @@
 
 ./gradlew clean build
 
-docker stop $(docker ps -a -q)
-docker rm $(docker ps -a -q)
+docker stop waitime_container1
+docker rm waitime_container1
 
 docker build -f Dockerfile -t waittime/java-backend:1 .
 docker run -d --name="waitime_container1" -p 80:8080 -p 443:8443 waittime/java-backend:1
