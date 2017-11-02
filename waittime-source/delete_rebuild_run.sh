20e81e0830e9378c9a20228223075f0a452608e7
Alain Brown <alain.brown@gmail.com>
2017-11-01 20:16:29 -0700

    added dummy data to waiting room api

--- waittime-source/delete_rebuild_run.sh
+++ waittime-source/delete_rebuild_run.sh
@@ -7,3 +7,4 @@ docker rm waitime_container1
 
 docker build -f Dockerfile -t waittime/java-backend:1 .
 docker run -d --name="waitime_container1" -p 80:8080 -p 443:8443 waittime/java-backend:1
+open http://localhost
