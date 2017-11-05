585a08eb6b3372da6404a8d566ade613b8ca0f57
james-srl <james@smartrhinolabs.com>
2017-11-04 23:05:06 -0700

    reorganizing the code a little bit, and fixing the SQL statements.

--- frontend/server.js
+++ frontend/server.js
@@ -1,10 +1,30 @@
 const server = require("server");
 const {get, post} = server.router;
+const db = require("./db");
 
-const port = process.env.PORT || 8080;
-
-// TODO set up socket stuff for real-time updates
-
-server({port}).then((ctx) => {
-    console.log(`Running frontend server on port ${port}. Hit Ctrl-C to quit...`);
+db.connectWithRetries(5).then(function(db) {
+    console.log("Connected to database. Starting server...");
+    const port = process.env.PORT || 8080;
+    
+    // TODO set up socket stuff for real-time updates
+    
+    server({port}, [
+        get('/db-test', (ctx) => {
+            console.log("Hit test endpoint.");
+    
+            db.query("SELECT * FROM dim_room_status").then((result) => {
+                console.log("Results: ", result);
+            }, (error) => {
+                console.error("Error querying database: ", error);
+            });
+    
+            return 'You made it!';
+        })
+    ]).then((ctx) => {
+        console.log(`Running frontend server on port ${port}. Hit Ctrl-C to quit...`);
+    });
+}, function(error) {
+    console.error("Problem connecting with database: ", error);
+}).catch(function(error) {
+    console.error("Exception connecting with database: ", error);
 });
