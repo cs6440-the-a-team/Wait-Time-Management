9a195a96f0477cb0f993e0eeabbc23cb0b3fb45a
james-srl <james@smartrhinolabs.com>
2017-11-07 22:39:18 -0800

    Adding in admin config pages

--- frontend/server.js
+++ frontend/server.js
@@ -1,30 +1,21 @@
 const server = require("server");
 const {get, post} = server.router;
-const db = require("./db");
+const {render} = server.reply;
 
-db.connectWithRetries(5).then(function(db) {
-    console.log("Connected to database. Starting server...");
-    const port = process.env.PORT || 8080;
-    
-    // TODO set up socket stuff for real-time updates
-    
-    server({port}, [
-        get('/db-test', (ctx) => {
-            console.log("Hit test endpoint.");
-    
-            db.query("SELECT * FROM dim_room_status").then((result) => {
-                console.log("Results: ", result);
-            }, (error) => {
-                console.error("Error querying database: ", error);
-            });
-    
-            return 'You made it!';
-        })
-    ]).then((ctx) => {
-        console.log(`Running frontend server on port ${port}. Hit Ctrl-C to quit...`);
-    });
-}, function(error) {
-    console.error("Problem connecting with database: ", error);
-}).catch(function(error) {
-    console.error("Exception connecting with database: ", error);
+
+const port = process.env.PORT || 8080;
+
+// TODO set up socket stuff for real-time updates
+
+server({port}, [
+    get("/admin*", ctx => {
+        //console.log("Got the admin page...");
+        return render("admin.html");
+    }),
+    get("/*", ctx => {
+        //console.log("Got the base page...");
+        return render("patient.html");
+    })
+]).then((ctx) => {
+    console.log(`Running frontend server on port ${port}. Hit Ctrl-C to quit...`);
 });
