51fe8bb12deeb8190d82d22d93da5d25cedfb8fc
james-srl <james@smartrhinolabs.com>
2017-11-07 23:25:22 -0800

    Allowing production builds

--- frontend/server.js
+++ frontend/server.js
@@ -2,19 +2,19 @@ const server = require("server");
 const {get, post} = server.router;
 const {render} = server.reply;
 
-
 const port = process.env.PORT || 8080;
+const env = process.env.NODE_ENV || 'development';
 
 // TODO set up socket stuff for real-time updates
 
 server({port}, [
     get("/admin*", ctx => {
-        //console.log("Got the admin page...");
-        return render("admin.html");
+        //console.log("Got the admin page: ", env);
+        return render("admin.hbs", {prod: env === "production"});
     }),
     get("/*", ctx => {
         //console.log("Got the base page...");
-        return render("patient.html");
+        return render("patient.hbs", {prod: env === "production"});
     })
 ]).then((ctx) => {
     console.log(`Running frontend server on port ${port}. Hit Ctrl-C to quit...`);
