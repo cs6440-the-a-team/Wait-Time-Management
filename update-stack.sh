5d8c0fdb6bf137aaae6b675157b5b661bc0a51e8
james-srl <james@smartrhinolabs.com>
2017-11-11 01:01:07 -0800

    Fixing endpoint for public interface

--- update-stack.sh
+++ update-stack.sh
@@ -4,5 +4,7 @@ echo "Getting new code from github.gatech.edu"
 git pull origin
 echo "Shutting down services..."
 sudo docker-compose down
+echo "Removing images with tag of <none>"
+sudo docker rmi $(sudo docker images | grep "<none>" | tr -s " " | cut -d " " -f 3)
 echo "Rebuilding services and starting again..."
 sudo docker-compose up --build --force-recreate -d
